cf8cb3da14561790b2821868a7afb1e2616c0545
Climate AmanteDeLaVida <git.climate@amante.co>
2017-07-13 17:59:00 -0700

    documentation: updated with simple version template
    Signed-off-by: Climate AmanteDeLaVida <git.climate@amante.co>

--- bash/bash.function.check.os.system.sh
+++ bash/bash.function.check.os.system.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Attach drive image with password
-# Tested With 10.11.6
-# Last updated at 06-22-2017
-# bash.osx.attach.drive.image.v.0.0.1 (http://ResearchRanks.com.)
-# Copyright 2011-2015 Research Ranks, LLC.
-# Licensed under MIT
+# bash.function.check.os.system
+# version: v.0.0.1
+# Updated: 07.13.2017
 
 function check_os_system() {
 	cat /etc/*-release
